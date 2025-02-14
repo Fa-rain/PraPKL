8ff60ad96ed53206eb5c7880281f48671bb5722c
Tegar <tegarrasyid6@gmail.com>
2025-02-14 13:42:53 +0700

    Add files via upload
    Kode Login

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    require "session.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Halo <?php echo $_SESSION['username'] ?></h2>
+    <?php echo "<a href='logout.php'>Logout</a>"; ?>
+</body>
+</html>
